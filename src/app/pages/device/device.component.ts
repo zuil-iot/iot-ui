e5ef8b9b98ef798ff605e2da1774b392a2faf0c2
Matt Bateman <mbateman@laughingsky.com>
2016-11-20 06:24:20 +0000

    added req_state and cur_state

--- src/app/pages/device/device.component.ts
+++ src/app/pages/device/device.component.ts
@@ -14,28 +14,64 @@ export class DeviceComponent implements OnInit {
 	private id: string;
 	private device: Device;
 	private pinList: any[];
+	private pinMap: any;
+	private name: string;
 
 	constructor(
 		private	route: ActivatedRoute,
 		private	devicesService:DevicesService
 	) { }
 
+	loadPinList(d) {
+		console.log( "device: ",JSON.stringify(d));
+		// Load Config
+		console.log( "config: ",JSON.stringify(d.config.pins));
+		var cnt=0;
+		for (var id in d.config.pins) {
+			var pin = d.config.pins[id];
+			pin.id = id;
+			pin.req_val = "-";
+			pin.cur_val = "-";
+			this.pinList.push(pin);
+			this.pinMap[id]=cnt++;
+		}
+		// Load Requested  Val
+		if (d.req_state.pins) {
+			console.log( "req_state: ",JSON.stringify(d.req_state.pins));
+			for (var id in d.req_state.pins) {
+				var v = d.req_state.pins[id].val;
+				var i = this.pinMap[id];
+				this.pinList[i].req_val = v;
+			}
+		}
+		// Load Current Val
+		if (d.cur_state.pins) {
+			console.log("cur_state: ", JSON.stringify(d.cur_state.pins));
+			for (var id in d.cur_state.pins) {
+				var v = d.cur_state.pins[id].val;
+				var i = this.pinMap[id];
+				this.pinList[i].cur_val = v;
+			}
+		}
+	}
+
 	ngOnInit() {
 		this.device = new Device();;
 		this.pinList = [];
+		this.pinMap = {};
+		this.name = this.id;
 		//when calling routes change
 		this.sub = this.route.params.subscribe(params=>{
 			this.id = params['id'];
 			this.devicesService.getOne(this.id)
 				.subscribe(d => {
-					this.device = d;
-					console.log( JSON.stringify(d.config.pins));
-					for (var id in d.config.pins) {
-						var pin = d.config.pins[id];
-						pin.id = id;
-						pin.val = "n/a";
-						this.pinList.push(pin);
+					if (d.alias) {
+						this.name = d.alias;
+					} else {
+						this.name = d.deviceID;
 					}
+					this.device = d;
+					this.loadPinList(d);
 				});
 		});
 	}
